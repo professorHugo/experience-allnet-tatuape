ea9a916d5d087bd7fd88b4c423653199c1a7e715
Hugo Christian <hugo@n2y.com.br>
2018-04-03 21:02:24 -0300

    Removido arquivo indevido

--- cnf/define.php
+++ cnf/define.php
@@ -1,13 +1,13 @@
 <?php
 //Off-line NOTEBOOK
-/*
+/**/
 define("HOST","localhost");
 define("USER","root");
 define("PASS","");
 define("DDB","experience");
 $conexao = mysql_connect(HOST, USER, PASS) or die (mysql_error);
 mysql_select_db(DDB, $conexao) or die (mysql_error);
-/**/
+/*
 //On-Line HOSTGATOR
 define("HOST","localhost");
 define("USUARIO","n2yco435_exp");
